d8fc4f62b74af1f8723459c99799b1329f2513d4
Crearns <qq5267439342016@163.com>
2019-11-11 20:19:01 +0800

    fix 服务无法注册 添加了安全配置类

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ ADD ./target/experiment-eureka-server-0.0.1-SNAPSHOT.jar /app/
 
 ENV username=""
 ENV password=""
+ENV hostname=""
 
 EXPOSE 8761
 
-ENTRYPOINT ["sh","-c","java -jar /app/experiment-eureka-server-0.0.1-SNAPSHOT.jar --spring.security.user.name=$username --spring.security.user.password=$password"]
+ENTRYPOINT ["sh","-c","java -jar /app/experiment-eureka-server-0.0.1-SNAPSHOT.jar --spring.security.user.name=$username --spring.security.user.password=$password --eureka.instance.hostname=$hostname"]
---
+++ src/main/java/com/bitnile/experimenteurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bitnile.experimenteurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created By Creams on 2019/11/11
+ */
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+
+}
